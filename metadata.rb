708846ff7d961b9ae05666bdef21254d0f01e909
m.benchchaoui@cloudbau.de <m.benchchaoui@cloudbau.de>
2013-11-25 16:48:31 +0100

    first commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'cookbook-openstack-common-wrapper'
+maintainer       'Mouad Benchchaoui'
+maintainer_email 'm.benchchaoui@cloudbau.de'
+license          ''
+description      'Installs/Configures cookbook-openstack-common-wrapper'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+
+depends 'openstack-common'
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: cookbook-openstack-common-wrapper
+# Recipe:: default
+#
+# Copyright 2013, Mouad Benchchaoui
+#
