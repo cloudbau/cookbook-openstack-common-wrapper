c27bdf2f234dbcc26bc9c01fbaf4eabf96224457
m.benchchaoui@cloudbau.de <m.benchchaoui@cloudbau.de>
2013-11-25 17:53:30 +0100

    Fix issue of keys that doesn't follow expected interface.

--- recipes/set-ip-by-interface.rb
+++ recipes/set-ip-by-interface.rb
@@ -4,18 +4,20 @@ include_recipe "openstack-common::set_endpoints_by_interface"
 db_interface = node['openstack']['db']['bind_interface']
 unless db_interface.nil?
   ip_address = address_for db_interface
-  components = node['openstack']['db'].select { |k, v| v.include? "host" }.keys
+  components = node['openstack']['db'].select { 
+    |k, v| v.include? "host" rescue false
+  }.keys
   components.each do |component|
     node.default['openstack']['db'][component]['host'] = ip_address
   end
 end
 
 # Fix rabbit host ip.
-mq_interface = node['mq']['bind_interface']
+mq_interface = node['openstack']['mq']['bind_interface']
 unless mq_interface.nil?
   ip_address = address_for mq_interface
   components = node['openstack'].select {
-      |k, v| v.include? "rabbit" and v["rabbit"].include? "host"
+    |k, v| (v.include? "rabbit" and v["rabbit"].include? "host") rescue false
   }.keys
   components.each do |component|
     node.default['openstack'][component]['rabbit']['host'] = ip_address
